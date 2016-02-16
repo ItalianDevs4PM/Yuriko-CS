881564a9d9beb9c5ead41a12b3ce7edb285a6ef7
Yuriko-CS <>
2016-02-16 22:18:17 +0100

    Updated code

---
+++ Yuriko-CS/Network/Packet.cs
@@ -0,0 +1,38 @@
+/*
+ *
+ * _   __          _ _                ____ ____  
+ *\ \ / /   _ _ __(_) | _____        / ___/ ___| 
+ * \ V / | | | '__| | |/ / _ \ _____| |   \___ \ 
+ *  | || |_| | |  | |   < (_) |_____| |___ ___) |
+ *  |_| \__,_|_|  |_|_|\_\___/       \____|____/ 
+ *
+ * Yuriko-CS, brand a new Minecraft: Pocket Edition
+ * server software in C#
+ * Copyright 2016 ItalianDevs4PM.
+ * 
+ * Modifications and implementations of this software
+ * which are made by ItalianDevs4PM or affiliates/contributors
+ * are licensed under Creative Commons
+ * Attribution-NonCommercial-NoDerivatives 4.0
+ * International License.
+ *
+ *
+ * @author ItalianDevs4PM
+ * @link   http://github.com/ItalianDevs4PM
+ *
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace YurikoCS {
+	interface Packet {
+
+		byte getID();
+
+		byte[] getContent();
+		
+	}
+}
+
